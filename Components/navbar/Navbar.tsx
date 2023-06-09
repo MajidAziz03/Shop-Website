5f9b00108caa0637e6ae626ef883634368f97dc4
Majid Aziz <majidaziz7ctech@gmail.com>
2023-06-09 18:46:23 +0500

    navbar left side

--- Components/navbar/Navbar.tsx
+++ Components/navbar/Navbar.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import { Search } from '@mui/icons-material';
 import styles from './navbar.module.scss';
 
 const Navbar = () => {
     return (
         <div className={styles.navbar}>
-            
+            <div className={styles.left}>
+                <div className={styles.search}>
+                    <input type="text" placeholder='Search Product' />
+                    <span className={styles.iconSearch}><Search className={styles.searchIcon} /> </span>
+                </div>
+            </div>
+            <div className={styles.middle}></div>
+            <div className={styles.right}></div>
         </div>
     )
 }
 
-export default Navbar
+export default Navbar;
