9a9ddf609fbbd565f88e0e54b1f47c0282d36f61
Majid Aziz <majidaziz7ctech@gmail.com>
2023-06-09 19:39:05 +0500

    completed header

---
+++ Components/featured/Featured.tsx
@@ -0,0 +1,11 @@
+import styles from './featured.module.scss';
+
+const Featured = () => {
+    return (
+        <div className={styles.featured}>
+            
+        </div>
+    )
+}
+
+export default Featured;
--- Components/header/Header.tsx
+++ Components/header/Header.tsx
@@ -1,9 +1,28 @@
 import styles from './header.module.scss';
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
 
 const Header = () => {
     return (
         <div className={styles.header}>
-
+            <Swiper
+                navigation={true}
+                modules={[Navigation]}
+                className={styles.mySwiper}
+            >
+                <SwiperSlide className={styles.slide}>
+                    <img src="https://images.pexels.com/photos/7615618/pexels-photo-7615618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                </SwiperSlide>
+                <SwiperSlide className={styles.slide}>
+                    <img src="https://images.pexels.com/photos/7615618/pexels-photo-7615618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                </SwiperSlide>
+                <SwiperSlide className={styles.slide}>
+                    <img src="https://images.pexels.com/photos/7615618/pexels-photo-7615618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                </SwiperSlide>
+            </Swiper>
         </div>
     )
 }
