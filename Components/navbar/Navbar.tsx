f7a0ee0193d3358d61e05766622de6251f4a414f
Majid Aziz <majidaziz7ctech@gmail.com>
2023-06-09 19:02:33 +0500

    navbar completed

--- Components/navbar/Navbar.tsx
+++ Components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Search } from '@mui/icons-material';
+import { Search, ShoppingCart } from '@mui/icons-material';
 import styles from './navbar.module.scss';
 
 const Navbar = () => {
@@ -10,8 +10,19 @@ const Navbar = () => {
                     <span className={styles.iconSearch}><Search className={styles.searchIcon} /> </span>
                 </div>
             </div>
-            <div className={styles.middle}></div>
-            <div className={styles.right}></div>
+            <div className={styles.middle}>
+                <h3> LOGO. </h3>
+            </div>
+            <div className={styles.right}>
+                <span className={styles.buttonsRight}> LOGIN </span>
+                <span className={styles.buttonsRight}> SIGNUP </span>
+                <div className={styles.logoRight}>
+                    <ShoppingCart className={styles.iconRight} />
+                    <div className={styles.number}>  
+                    <span> 3 </span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
