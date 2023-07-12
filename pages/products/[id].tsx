8c2870aed340490b12d70852651b93516d7d6162
Majid Aziz <majidaziz7ctech@gmail.com>
2023-07-12 16:15:47 +0500

    signle product rending servier side

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -5,22 +5,8 @@ import axios from 'axios';
 import BaseLayout from '@/Components/BaseLayout';
 import FsLightbox from "fslightbox-react";
 
-const SingleProductPage = () => {
-    const router = useRouter();
-    const [product, setProduct] = useState<any>({})
+const SingleProductPage = ({singleProduct} : any) => {
     const [toggler, setToggler] = useState(false);
-
-    const id = router.query && router.query.handle;
-
-    useEffect(() => {
-        const get_product = async () => {
-            const res = await axios.get(`https://dummyjson.com/products/${id}`)
-            if (res) {
-                setProduct(res.data)
-            }
-        }
-        get_product()
-    }, [])
 
     return (
         <BaseLayout>
@@ -28,7 +14,7 @@ const SingleProductPage = () => {
                 <div className={styles.left}>
                     <div className={styles.leftWrapper}>
                         <div className={styles.img_wrapper}>
-                            {product.images?.slice(0, 3).map((item: any) => {
+                            {singleProduct.images.map((item: any) => {
                                 return (
                                     <>
                                         <img src={item} alt="" className={styles.singleImg} />
@@ -36,32 +22,41 @@ const SingleProductPage = () => {
                                 )
                             })}
                         </div>
-                        <div className={styles.main_imgWrapper} onClick={() => setToggler(!toggler)} style={{cursor : "pointer"}}>
+                        <div className={styles.main_imgWrapper} onClick={() => setToggler(!toggler)} style={{ cursor: "pointer" }}>
                             <div className={styles.main_img}>
-                                <img src={product.thumbnail} alt="" />
+                                <img src={singleProduct.thumbnail} alt="" />
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className={styles.right}>
                     <div className={styles.info}>
-                        <h2 className={styles.title}> {product.title} </h2>
+                        <h2 className={styles.title}> {singleProduct.title} </h2>
                         <div className={styles.priceWrapper}>
-                            <span className={styles.price_original}> RS: {(product.price - (product.discountPercentage / 100)).toFixed(2)}  </span>
-                            <span className={styles.price_discount}> {product.price}  </span>
+                            <span className={styles.price_original}> RS: {(singleProduct.price - (singleProduct.discountPercentage / 100)).toFixed(2)}  </span>
+                            <span className={styles.price_discount}> {singleProduct.price}  </span>
                         </div>
-                        <p className={styles.description}> {product.description} </p>
+                        <p className={styles.description}> {singleProduct.description} </p>
                         <button className={styles.cart_btn}> Add to cart </button>
                     </div>
                 </div>
             </div>
             <FsLightbox
-                    toggler={toggler}
-                    sources={product.images?.map((item : any) => item)}
-                    type="image"
+                toggler={toggler}
+                sources={singleProduct.images.map((item: any) => item)}
+                type="image"
             />
         </BaseLayout>
     )
+}
+
+export const getServerSideProps = async ({params} : any) => {
+    const res = await fetch(`https://dummyjson.com/products/${params.id}`).then(data => data.json())
+    return {
+        props : {
+            singleProduct : res,
+        }
+    }
 }
 
 export default SingleProductPage;
